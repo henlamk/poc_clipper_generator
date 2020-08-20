99bd07659d371667a7d02c92b7c5056eca0bc346
henlamk <hendrik.lakaemper@gmail.com>
2020-08-20 23:34:39 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # poc_clipper_generator
+
+This is just a project to test how multiple randomly generated clippath widgets stacked on each other work out.
---
+++ android/app/src/main/kotlin/com/example/poc_clipper_generator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.poc_clipper_generator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
